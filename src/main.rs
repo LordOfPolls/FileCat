a630093a3a0bbef4f8a071ff40613968f9992824
LordOfPolls <dev@lordofpolls.com>
2024-03-08 07:50:22 +0000

    feat: add additional comment specs

--- src/main.rs
+++ src/main.rs
@@ -36,22 +36,19 @@ fn collect_files_with_extension(
 
 fn get_comment_syntax(extension: &str) -> (&str, &str) {
     match extension {
-        "py" | "sh" | "rb" | "pl" | "r" | "jl" => ("#", ""),
-        "c" | "cpp" | "h" | "hpp" | "java" | "js" | "ts" | "go" | "php" | "swift" | "kt" | "rs" | "fs" | "fsx" | "fsi" | "cs" | "dart" | "scala" | "groovy" | "v" | "hs" | "elm" | "erl" | "hrl" => ("//", ""), // could probably just leave this for the default case, but it's nice to be explicit
-        "html" | "fsproj" | "xml" | "svg" | "xhtml" | "xaml" | "aspx" | "jsp" | "jspx" | "gsp" => ("<!--", "-->"),
+        "py" | "sh" | "rb" | "pl" | "r" | "jl" | "gnuplot" | "toml" | "ini" | "cfg" | "conf" | "properties" | "yaml" | "yml" | "hcl" | "tf" | "nix" => ("#", ""),
+        "c" | "cpp" | "h" | "hpp" | "java" | "js" | "ts" | "go" | "php" | "swift" | "kt" | "rs" | "fs" | "fsx" | "fsi" | "cs" | "dart" | "scala" | "groovy" | "v" | "hs" | "elm" | "erl" | "hrl" | "asciidoc" | "adoc" | "json" | "jsonc" | "cson" | "proto" | "hocon" | "json5" | "hjson" => ("//", ""),
+        "html" | "fsproj" | "xml" | "svg" | "xhtml" | "xaml" | "aspx" | "jsp" | "jspx" | "gsp" | "xsd" | "dtd" | "xsl" | "xslt" | "mathml" => ("<!--", "-->"),
         "css" | "scss" | "sass" | "less" | "stylus" => ("/*", "*/"),
-        "lua" | "sql" | "ada" | "applescript" | "hive" | "pig" | "vb" => ("--", ""),
+        "lua" | "sql" | "ada" | "applescript" | "hive" | "pig" | "vb" | "dhall" => ("--", ""),
         "coffee" | "litcoffee" => ("###", ""),
         "nim" => ("##", ""),
-        "edn" | "clj" | "cljs" | "cljc" => (";", ""),
+        "edn" | "clj" | "cljs" | "cljc" | "s" | "S" | "inc" | "ahk" | "scm" |
+        "sch" | "rkt" | "sld" => (";", ""),
         "ml" | "mli" | "fsscript" => ("(*", "*)"),
-        "s" | "S" | "inc" => (";", ""),
-        "ahk" => (";", ""),
         "tex" | "sty" => ("%", ""),
-        "asciidoc" | "adoc" => ("//", ""),
-        "gnuplot" => ("#", ""),
-        "scm" | "sch" | "rkt" | "sld" => (";", ""),
         "m4" => ("dnl", ""),
+        "csv" | "tsv" => ("", ""),
         _ => ("//", "")  // default to C-style comments
     }
 }
